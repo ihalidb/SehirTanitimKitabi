df87b9d1c3dc49ef8e1223fcb06cc8b66e73bb9a
İbrhaim Halid Bayrak <ibrahimh.bayrak@gmail.com>
2023-07-30 16:50:02 +0300

    Initial Commit

---
+++ SehirTanitimKitabi/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SehirTanitimKitabi
+//
+//  Created by İbrahim Halid Bayrak on 30.07.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
