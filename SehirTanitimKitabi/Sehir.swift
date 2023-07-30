bad44515aabab348ac357691f86b72f175125c5c
İbrhaim Halid Bayrak <ibrahimh.bayrak@gmail.com>
2023-07-30 18:16:00 +0300

    sınıflar kullanılarak kitap oluşturuldu

---
+++ SehirTanitimKitabi/Sehir.swift
@@ -0,0 +1,24 @@
+//
+//  Sehir.swift
+//  SehirTanitimKitabi
+//
+//  Created by İbrahim Halid Bayrak on 30.07.2023.
+//
+
+import Foundation
+import UIKit
+
+class Sehir {
+    
+    var isim : String
+    var bolge :String
+    var gorsel : UIImage
+    
+    init(isim: String, bolge: String, gorsel: UIImage) {
+        self.isim = isim
+        self.bolge = bolge
+        self.gorsel = gorsel
+    }
+    
+    
+}
